66fe17aafb39a000a9bd5e4a1ecfdce046e216f4
CuongDD <cuongdd@sandinh.net>
2017-03-18 09:42:29 +0700

    day 4 in 30 days of code

---
+++ src/code/Person.java
@@ -0,0 +1,28 @@
+package code;
+
+/**
+ * Created by cuongdd2@gmail.com on 18-Mar-17.
+ */
+public class Person {
+    private int age;
+
+    public Person(int initialAge) {
+        if (initialAge >= 0) this.age = initialAge;
+        else {
+            this.age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+    }
+
+    public void yearPasses() {
+        this.age++;
+    }
+
+    public void amIOld() {
+        String msg;
+        if (this.age < 13) msg = "You are young.";
+        else if (this.age >= 18) msg = "You are old.";
+        else msg = "You are a teenager.";
+        System.out.println(msg);
+    }
+}
