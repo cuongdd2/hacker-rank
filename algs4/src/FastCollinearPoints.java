3b517d5121282922b9c26dafb26072a9e5bd2415
CuongDD <cuongdd@sandinh.net>
2017-06-06 16:25:19 +0700

    misc

---
+++ algs4/src/FastCollinearPoints.java
@@ -0,0 +1,16 @@
+/**
+ * Created by cuongdd2@gmail.com on 10-Apr-17.
+ */
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    public int numerOfSegments() {
+        return 0;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+}
